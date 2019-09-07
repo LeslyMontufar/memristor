28aaeae58a4e319d18f75ab903bc621b06049e2c
lesly <leslymontufarj@gmail.com>
2019-09-07 16:30:06 -0300

    Finished poster

--- vxi.m
+++ vxi.m
@@ -1,15 +1,30 @@
-  t = -10:0.1:10;
-  v0 = 1;
-  
-%  Typical parameters
-  t0 = 0.1*10^(-3);
-  w0 = 50*10^3;
-  
-  
-  %freq = 60;
-  %freq = 1/(2*pi);
-  %w = 2*pi*freq;
-  
-  v = v0*sin(w*t);
-  flux = v0*(1-cos(w*t))/w;
-  plot(t, v, 'b',t, flux, 'r');
+clear all;
+close all;
+
+% MATLAB code for simulating a memristor 
+v0=1; % amplitude of the input wave 
+omega=3; % frequency (rad/s) 
+MD=1e-14;%drift velocity 
+ROFF=16e+3; % resistance of undoped region 
+RON=100;% resistance of the doped region 
+r=ROFF/RON; 
+%D = 10e-9; % Width of Memristor 
+w0 = 1e-9; % Width of doped region  
+%M0=(RON*(w0/D))+(ROFF*(1-(w0/D))); 
+%q0=(D^2)/(MD*RON); 
+deltaR=ROFF-RON; 
+
+
+D=3e-9:0.1e-9:10e-9;
+q0=(D.^2)/(MD*RON); 
+Mt = (RON*(w0./D))+(ROFF*(1-(w0./D))) + (deltaR)*1./q0;
+
+
+
+figure(1) 
+plot(D, Mt, 'LineWidth', 2);%Plot of charge versus flux 
+%title('Flux-charge curve');
+grid on;
+xlabel('D');
+ylabel('memristencia');
+
